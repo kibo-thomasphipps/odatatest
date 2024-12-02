0a84359798c3731bd7e4a361fca7fb4e564dbf6e
kibo-kevinwatts <kevin.watts@kibocommerce.com>
2024-12-02 12:40:15 -0600

    ts setup

--- odata2ts.config.ts
+++ odata2ts.config.ts
@@ -1,15 +1,15 @@
-import { ConfigFileOptions, EmitModes, Modes  } from "@odata2ts/odata2ts";
+import { ConfigFileOptions, EmitModes, Modes } from "@odata2ts/odata2ts";
 
 const config: ConfigFileOptions = {
-  mode: Modes.qobjects,
+  mode: Modes.all,
   skipEditableModels: false,
   skipIdModels: false,
   skipOperations: false,
   services: {
-    trippin: {
+    channeladvisor: {
       sourceUrl: "https://services.odata.org/TripPinRESTierService",
-      source: "resource/trippin.xml",
-      output: "build/trippin",
+      source: "resource/channeladvisor.xml",
+      output: "src/odata",
     }
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,15 @@
 export * from './lib/async';
 export * from './lib/number';
+import { createQueryBuilderV4 } from "@odata2ts/odata-query-builder"
+import { qOrder } from './odata/QChannelAdvisorRestApiV1Models'
+import { OrderService } from './odata/ChannelAdvisorRestApiV1ModelsService';
+
+const q = createQueryBuilderV4("Order", qOrder)
+    .select("CheckoutStatus") // => typesafe: only model attributes are allowed
+    .filter(qOrder.CheckoutStatus.eq("Completed")) // => typesafe: only model attributes are allowed
+    .expand("Items") // => typesafe: only expandable properties are allowed
+    .build();
+// const client = OData.New4({ serviceEndpoint: "/odata" })
+// const service = new OrderService(fetch, "/odata", "name", { headers: {}});
+
+console.log(q)
